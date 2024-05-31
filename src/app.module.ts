ea304319b567dae1c7828e5cdf0061ac29e78124
brunosm16 <brunoskr23@gmail.com>
2024-05-31 20:05:26 -0300

    refactor: db running

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'test',
+      database: 'nest-paginator-data',
       autoLoadEntities: true,
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [UsersController],
   providers: [],
